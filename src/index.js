211fc2642010ca938281946007b682dfc0b7917f
“fatimasham” <diel1989@mail.ru>
2024-10-08 22:10:59 +0500

    first commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,83 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import cartReducer from './reducers/cartReducer';
+import { configureStore } from '@reduxjs/toolkit';
+import { addToCart, removeItem } from './actions';
+
+
+let store = configureStore(cartReducer);
+store.subscribe(() => console.log(store.getState()))
+
+store.dispatch(addToCart());
+store.dispatch(addToCart());
+store.dispatch(addToCart());
+store.dispatch(removeItem())
+
+
+
+
+
+// 1 - STORE - ALL DATA
+
+
+
+// 2 - ACTION
+
+// A 
+//const addToCart = () => {
+  //return {
+    //type: "ADD_TO_CART"
+  //}
+//}
+
+// B
+//const removeItem = () => {
+  //return {
+    //type: "REMOVE_ITEM"
+  //}
+//}
+
+//C
+//const changeNumber = () => {
+  //return {
+    //type: "CHANGE_NUMBER"
+  //}
+//}
+
+
+// 3 - REDUCER - ОПИСАНИЕ ТОГО КАК НАШИ НАМЕРЕНИЯ СОВЕРШИТЬ ДЕЙСТВИЯ ПОМЕНЯЮТ СОСТОЯНИЕ
+
+//const cart = (state = 0, action) => {
+  //switch(action.type) {
+    //case 'ADD_TO_CART':
+    //return state + 1;
+
+    //case 'REMOVE_ITEM':
+    //return state - 1;
+  //}
+//}
+
+//let store = createStore(cart);
+//store.subscribe(() => console.log(store.getState()))
+
+// 4 - DISPATCH
+
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(addToCart())
+//store.dispatch(removeItem())
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
